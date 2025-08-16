c2913026c0418c122b0fcd1c8a9bf7960fdf58a2
bataa7 <liuyanbo1204@163.com>
2025-08-16 15:34:35 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -137,7 +137,7 @@ const config = {
 	//项目卡片 其中 字段"show"控制初始卡片的text是否展开
 	projectcards: [
 		{ go: "🚀 前往", img: "/img/xiyang.jpg", title: "博客", subtitle: "我不会写", text: "If you see this line, I've managed to get your attention.", url: "", show: false },
-		{ go: "🗂️ 前往", img: "/img/yejing.jpg", title: "本站数据报表", subtitle: "让数据说话", text: "If you see this line, I've managed to get your attention.", url: "https://v6.51.la/report/overview?comId=3083744", show: false },
+		{ go: "🗂️ 前往", img: "/img/yejing.jpg", title: "本站数据报表", subtitle: "让数据说话", text: "If you see this line, I've managed to get your attention.", url: "https://v6.51.la/s/w0G4Ub5rfK19gvy", show: false },
 		{ go: "📝 前往", img: "/img/sunshine.jpg", title: "赛博要饭", subtitle: "科技化缘，一码在手天下我有", text: "If you see this line, I've managed to get your attention.", url: "https://search.bilibili.com/", show: false },
 	],
 
